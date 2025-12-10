13dff24e7d3b818a760ee8356b674721c2704a1b
dreamforSh <3856889129@qq.com>
2025-12-10 16:43:30 +0800

    bugfix区块或丢失

--- src/main/java/dev/doctor4t/trainmurdermystery/cca/AreasWorldComponent.java
+++ src/main/java/dev/doctor4t/trainmurdermystery/cca/AreasWorldComponent.java
@@ -82,11 +82,10 @@ public class AreasWorldComponent implements AutoSyncedComponent {
 
     Box readyArea = new Box(-1017, -1, -363.5f, -813, 3, -357.5f);
     Vec3d playAreaOffset = new Vec3d(963, 121, -175);
-    Box playArea = new Box(-82, 60, -546, 177, 84, -524);
+    Box playArea = new Box(177 ,60 ,-524, -82 ,84, -546);
 
-
-    Box resetTemplateArea = new Box(-57, 64, -541, 177, 74, -531);
-    Box resetPasteArea = new Box(-82, 119, -546, 177, 139, -524);
+    Box resetTemplateArea = new Box(177 ,60 ,-524, -82 ,84, -546);
+    Box resetPasteArea = resetTemplateArea.offset(0, 55, 0);
     public PosWithOrientation getSpawnPos() {
         return spawnPos;
     }
@@ -160,9 +159,8 @@ public class AreasWorldComponent implements AutoSyncedComponent {
         this.playAreaOffset = getVec3dFromNbt(tag, "playAreaOffset");
         this.playArea = getBoxFromNbt(tag, "playArea");
 
-        // 修复：使用正确的模板区域坐标
-        this.resetTemplateArea = new Box(-57, 64, -541, 177, 74, -531);
-        this.resetPasteArea = new Box(-82, 119, -546, 177, 139, -524);
+        this.resetTemplateArea = new Box(177 ,60 ,-524, -82 ,84, -546);
+        this.resetPasteArea = resetTemplateArea.offset(0, 55, 0);
     }
 
     @Override
