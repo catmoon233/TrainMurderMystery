9285a735db96d8d579fdf7b8e7077a6fb914b880
wifi-left <ysy1679682327@outlook.com>
2026-02-02 15:27:31 +0800

    Sync when taking skins

--- src/main/java/dev/doctor4t/trainmurdermystery/command/ManageSkinsCommand.java
+++ src/main/java/dev/doctor4t/trainmurdermystery/command/ManageSkinsCommand.java
@@ -1,10 +1,8 @@
 package dev.doctor4t.trainmurdermystery.command;
 
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import com.mojang.brigadier.suggestion.SuggestionProvider;
 import dev.doctor4t.trainmurdermystery.cca.PlayerSkinsComponent;
 import dev.doctor4t.trainmurdermystery.command.argument.SkinArgumentType;
 import dev.doctor4t.trainmurdermystery.util.SkinManager;
@@ -25,33 +23,30 @@ public class ManageSkinsCommand {
 
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher, CommandBuildContext context) {
 
-
         dispatcher.register(Commands.literal("tmm:manageskins")
-            .requires(source -> source.hasPermission(2)) // 需要权限等级2
-            .then(Commands.literal("give")
-                .then(Commands.argument("targets", EntityArgument.players())
-                    .then(Commands.argument("item", ItemArgument.item(context))
-                        .then(Commands.argument("skin", SkinArgumentType.string())
-                            .executes(ManageSkinsCommand::giveSkin)))))
-            .then(Commands.literal("take")
-                .then(Commands.argument("targets", EntityArgument.players())
-                    .then(Commands.argument("item", ItemArgument.item(context))
-                        .then(Commands.argument("skin", SkinArgumentType.string())
-                            .executes(ManageSkinsCommand::takeSkin)))))
-            .then(Commands.literal("setequipped")
-                .then(Commands.argument("targets", EntityArgument.players())
-                    .then(Commands.argument("item", ItemArgument.item(context))
-                        .then(Commands.argument("skin", SkinArgumentType.string())
-                            .executes(ManageSkinsCommand::setEquippedSkin)))))
-            .then(Commands.literal("list")
-                .then(Commands.argument("targets", EntityArgument.players())
-                    .executes(ManageSkinsCommand::listSkins)))
-        );
+                .requires(source -> source.hasPermission(2)) // 需要权限等级2
+                .then(Commands.literal("give")
+                        .then(Commands.argument("targets", EntityArgument.players())
+                                .then(Commands.argument("item", ItemArgument.item(context))
+                                        .then(Commands.argument("skin", SkinArgumentType.string())
+                                                .executes(ManageSkinsCommand::giveSkin)))))
+                .then(Commands.literal("take")
+                        .then(Commands.argument("targets", EntityArgument.players())
+                                .then(Commands.argument("item", ItemArgument.item(context))
+                                        .then(Commands.argument("skin", SkinArgumentType.string())
+                                                .executes(ManageSkinsCommand::takeSkin)))))
+                .then(Commands.literal("setequipped")
+                        .then(Commands.argument("targets", EntityArgument.players())
+                                .then(Commands.argument("item", ItemArgument.item(context))
+                                        .then(Commands.argument("skin", SkinArgumentType.string())
+                                                .executes(ManageSkinsCommand::setEquippedSkin)))))
+                .then(Commands.literal("list")
+                        .then(Commands.argument("targets", EntityArgument.players())
+                                .executes(ManageSkinsCommand::listSkins))));
     }
 
     private static int giveSkin(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
         try {
-
 
             Collection<? extends Player> players = EntityArgument.getPlayers(context, "targets");
             var item = ItemArgument.getItem(context, "item");
@@ -60,15 +55,16 @@ public class ManageSkinsCommand {
             int successes = 0;
             for (Player player : players) {
                 // 解锁指定物品类型的皮肤
-                SkinManager.unlockSkinForItemType(player, BuiltInRegistries.ITEM.getKey(item.getItem()).toString(), skin);
-                context.getSource().sendSuccess(() -> Component.literal("Gave skin '" + skin + "' for item type '" + item + "' to " + player.getName().getString()), true);
+                SkinManager.unlockSkinForItemType(player, BuiltInRegistries.ITEM.getKey(item.getItem()).toString(),
+                        skin);
+                context.getSource().sendSuccess(() -> Component.literal(
+                        "Gave skin '" + skin + "' for item type '" + item + "' to " + player.getName().getString()),
+                        true);
                 successes++;
             }
 
             return successes;
-        }catch (
-                Exception ig
-        ){
+        } catch (Exception ig) {
             LOGGER.error("Error occurred while executing manageskins command", ig);
             return 0;
         }
@@ -84,7 +80,10 @@ public class ManageSkinsCommand {
             // 锁定（移除）指定物品类型的皮肤
             PlayerSkinsComponent skinsComponent = PlayerSkinsComponent.KEY.get(player);
             skinsComponent.lockSkinForItemType(BuiltInRegistries.ITEM.getKey(item.getItem()).toString(), skin);
-            context.getSource().sendSuccess(() -> Component.literal("Removed skin '" + skin + "' for item type '" + item + "' from " + player.getName().getString()), true);
+            context.getSource().sendSuccess(() -> Component.literal(
+                    "Removed skin '" + skin + "' for item type '" + item + "' from " + player.getName().getString()),
+                    true);
+            skinsComponent.syncSkinsToClient();
             successes++;
         }
 
@@ -99,8 +98,10 @@ public class ManageSkinsCommand {
         int successes = 0;
         for (Player player : players) {
             // 设置指定物品类型的装备皮肤
-            SkinManager.setEquippedSkinForItemType(player, BuiltInRegistries.ITEM.getKey(item.getItem()).toString(), skin);
-            context.getSource().sendSuccess(() -> Component.literal("Set equipped skin to '" + skin + "' for item type '" + item + "' for " + player.getName().getString()), true);
+            SkinManager.setEquippedSkinForItemType(player, BuiltInRegistries.ITEM.getKey(item.getItem()).toString(),
+                    skin);
+            context.getSource().sendSuccess(() -> Component.literal("Set equipped skin to '" + skin
+                    + "' for item type '" + item + "' for " + player.getName().getString()), true);
             successes++;
         }
 
@@ -113,12 +114,12 @@ public class ManageSkinsCommand {
         for (Player player : players) {
             PlayerSkinsComponent skinsComponent = PlayerSkinsComponent.KEY.get(player);
             StringBuilder result = new StringBuilder("Skins for " + player.getName().getString() + ":\n");
-            
+
             // 显示已装备的皮肤
             result.append("Equipped skins:\n");
-            skinsComponent.getEquippedSkins().forEach((item, skin) -> 
-                result.append("  ").append(item).append(" -> ").append(skin).append("\n"));
-            
+            skinsComponent.getEquippedSkins()
+                    .forEach((item, skin) -> result.append("  ").append(item).append(" -> ").append(skin).append("\n"));
+
             // 显示解锁的皮肤
             result.append("Unlocked skins:\n");
             skinsComponent.getUnlockedSkins().forEach((item, skins) -> {
@@ -129,7 +130,7 @@ public class ManageSkinsCommand {
                     }
                 });
             });
-            
+
             context.getSource().sendSuccess(() -> Component.literal(result.toString()), false);
         }
 
