d2dfe66ca61dbbd4cceeae4aa0bfcfb5d07addb2
wifi-left <ysy1679682327@outlook.com>
2026-02-02 15:53:37 +0800

    update

--- src/main/java/dev/doctor4t/trainmurdermystery/TMM.java
+++ src/main/java/dev/doctor4t/trainmurdermystery/TMM.java
@@ -63,10 +63,7 @@ public class TMM implements ModInitializer {
     public static List<Predicate<Role>> canUseChatHud = new ArrayList<>();
     public static List<Predicate<Player>> canCollide = new ArrayList<>();
     public static List<Predicate<Entity>> canCollideEntity = new ArrayList<>();
-    public static ArrayList<String> canDropItem = new ArrayList<>(List.of(
-            "exposure:album",
-            "exposure:photograph",
-            "noellesroles:mint_candies"));
+    public static ArrayList<String> canDropItem = new ArrayList<>();
 
     public static @NotNull ResourceLocation id(String name) {
         return ResourceLocation.fromNamespaceAndPath(MOD_ID, name);
