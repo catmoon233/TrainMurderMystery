7dd77458a97c7a20016f147053582212bd6bf8db
canyuesama <3445829286@qq.com>
2026-02-05 21:54:18 +0800

    refactor(whitelist): 移除模组白名单相关注入逻辑并添加新的混入类
    - 注释掉 ClientIntentionPacketMixin 中的初始化、网络读取和写入方法注入
    - 注释掉 ServerHandshakePacketListenerImplMixin 中的模组列表匹配检查逻辑
    - 新增 MutilPlayerScreenMixin 空混入类
    - 在 trainmurdermystery.mixins.json 配置文件中注册新的混入类

---
+++ src/main/java/dev/doctor4t/trainmurdermystery/mixin/client/MutilPlayerScreenMixin.java
@@ -0,0 +1,9 @@
+package dev.doctor4t.trainmurdermystery.mixin.client;
+
+import net.minecraft.client.gui.screens.multiplayer.JoinMultiplayerScreen;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(JoinMultiplayerScreen.class)
+public class MutilPlayerScreenMixin {
+
+}
