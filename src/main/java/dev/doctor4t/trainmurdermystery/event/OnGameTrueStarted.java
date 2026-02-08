4dc29c74cde9c73bb4fdda1c9a069d25106644cb
wifi-left <ysy1679682327@outlook.com>
2026-02-08 19:08:05 +0800

    游戏开始事件

---
+++ src/main/java/dev/doctor4t/trainmurdermystery/event/OnGameTrueStarted.java
@@ -0,0 +1,24 @@
+package dev.doctor4t.trainmurdermystery.event;
+
+import net.fabricmc.fabric.api.event.Event;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.item.ItemStack;
+
+import static net.fabricmc.fabric.api.event.EventFactory.createArrayBacked;
+
+public interface OnGameTrueStarted {
+
+    /**
+     * Callback for determining whether an {@link ItemStack} should drop when player
+     * died
+     */
+    Event<OnGameTrueStarted> EVENT = createArrayBacked(OnGameTrueStarted.class,
+            listeners -> (sl) -> {
+                for (OnGameTrueStarted listener : listeners) {
+                    listener.onGameTrueStarted(sl);
+                }
+            });
+
+    void onGameTrueStarted(ServerLevel serverLevel);
+}
