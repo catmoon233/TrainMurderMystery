456b8f4d9446df6bbb3c9f65ac58ffa199e2d299
wifi-left <ysy1679682327@outlook.com>
2026-02-08 20:05:07 +0800

    cancelable

--- src/main/java/dev/doctor4t/trainmurdermystery/mixin/client/DisconnectedScreenMixin.java
+++ src/main/java/dev/doctor4t/trainmurdermystery/mixin/client/DisconnectedScreenMixin.java
@@ -4,13 +4,12 @@ import net.minecraft.client.gui.screens.DisconnectedScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(DisconnectedScreen.class)
 public class DisconnectedScreenMixin
 {
-    @Inject(method = "shouldCloseOnEsc", at = @At("HEAD"))
+    @Inject(method = "shouldCloseOnEsc", at = @At("HEAD"), cancellable = true)
     public void init(CallbackInfoReturnable<Boolean> cir)
     {
         cir.setReturnValue(true);
