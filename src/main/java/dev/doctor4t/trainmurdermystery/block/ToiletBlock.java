180d0eb7d216e7cf4f3e87a68e9feaed76faf422
wifi-left <ysy1679682327@outlook.com>
2026-02-13 12:28:20 +0800

    马桶修复

--- src/main/java/dev/doctor4t/trainmurdermystery/block/ToiletBlock.java
+++ src/main/java/dev/doctor4t/trainmurdermystery/block/ToiletBlock.java
@@ -1,17 +1,28 @@
 package dev.doctor4t.trainmurdermystery.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class ToiletBlock extends CouchBlock {
+    public static final VoxelShape SHAPE = Block.box(0, 0, 0, 16, 12, 16);
+
     public ToiletBlock(Properties settings) {
         super(settings);
     }
 
     @Override
     public Vec3 getNorthFacingSitPos(Level world, BlockState state, BlockPos pos) {
-        return new Vec3(0.5f, 1, 0.6f);
+        return new Vec3(0.5f, 0.4f, 0.5f);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
+        return SHAPE;
     }
 }
