6e61be7c874586ffc43e11d1d6d26860c8ee3dd3
wifi-left <ysy1679682327@outlook.com>
2026-02-19 13:05:17 +0800

    清除物品

--- src/main/java/dev/doctor4t/trainmurdermystery/util/TMMItemUtils.java
+++ src/main/java/dev/doctor4t/trainmurdermystery/util/TMMItemUtils.java
@@ -30,6 +30,52 @@ public class TMMItemUtils {
         return result;
     }
 
+    public static int clearItem(Player player, Item item, int count) {
+        Predicate<ItemStack> predicate = (itemStack) -> {
+            return itemStack.is(item);
+        };
+        int result = player.getInventory().clearOrCountMatchingItems(predicate, count,
+                player.inventoryMenu.getCraftSlots());
+        player.containerMenu.broadcastChanges();
+        player.inventoryMenu.slotsChanged(player.getInventory());
+        return result;
+    }
+
+    public static int clearItem(Player player, TagKey<Item> item, int count) {
+        Predicate<ItemStack> predicate = (itemStack) -> {
+            return itemStack.is(item);
+        };
+        int result = player.getInventory().clearOrCountMatchingItems(predicate, count,
+                player.inventoryMenu.getCraftSlots());
+        player.containerMenu.broadcastChanges();
+        player.inventoryMenu.slotsChanged(player.getInventory());
+        return result;
+    }
+
+    public static int clearItem(Player player, Predicate<ItemStack> predicate) {
+        int result = player.getInventory().clearOrCountMatchingItems(predicate, -1,
+                player.inventoryMenu.getCraftSlots());
+        player.containerMenu.broadcastChanges();
+        player.inventoryMenu.slotsChanged(player.getInventory());
+        return result;
+    }
+
+    public static int clearItem(Player player, Predicate<ItemStack> predicate, int count) {
+        int result = player.getInventory().clearOrCountMatchingItems(predicate, count,
+                player.inventoryMenu.getCraftSlots());
+        player.containerMenu.broadcastChanges();
+        player.inventoryMenu.slotsChanged(player.getInventory());
+        return result;
+    }
+
+    public static int hasItem(Player player, Predicate<ItemStack> predicate) {
+        int result = player.getInventory().clearOrCountMatchingItems(predicate, 0,
+                player.inventoryMenu.getCraftSlots());
+        // player.containerMenu.broadcastChanges();
+        // player.inventoryMenu.slotsChanged(player.getInventory());
+        return result;
+    }
+
     public static int hasItem(Player player, Item item) {
         Predicate<ItemStack> predicate = (itemStack) -> {
             return itemStack.is(item);
