7b2a7b7fd6934732bb7a7bf85e0532ab345364a9
wifi-left <ysy1679682327@outlook.com>
2026-02-27 20:45:47 +0800

    Beautify

--- src/main/java/dev/doctor4t/trainmurdermystery/client/gui/RoleNameRenderer.java
+++ src/main/java/dev/doctor4t/trainmurdermystery/client/gui/RoleNameRenderer.java
@@ -117,7 +117,8 @@ public class RoleNameRenderer {
                                     roleText1 = roleText2;
                                 }
                             }
-                            if (roleText1==null)return;
+                            if (roleText1 == null)
+                                return;
                             int roleWidth1 = renderer.width(roleText1);
                             context.drawString(renderer, roleText1, -roleWidth1 / 2, 0,
                                     Mth.color(1f, 0f, 0f) | ((int) (nametagAlpha * 255) << 24));
