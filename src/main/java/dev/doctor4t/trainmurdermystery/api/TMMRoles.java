1b50f532315215fb9c5c6e865d7f411858d045cf
wifi-left <ysy1679682327@outlook.com>
2026-03-05 16:59:09 +0800

    Make role list ordered

--- src/main/java/dev/doctor4t/trainmurdermystery/api/TMMRoles.java
+++ src/main/java/dev/doctor4t/trainmurdermystery/api/TMMRoles.java
@@ -4,29 +4,36 @@ import dev.doctor4t.trainmurdermystery.TMM;
 import dev.doctor4t.trainmurdermystery.client.gui.RoleAnnouncementTexts;
 import dev.doctor4t.trainmurdermystery.game.GameConstants;
 import net.minecraft.resources.ResourceLocation;
+
+import org.antlr.v4.misc.OrderedHashMap;
 import org.ladysnake.cca.api.v3.component.ComponentKey;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class TMMRoles {
-    public static final Map<ResourceLocation,Role> ROLES = new HashMap<>();
+    public static final Map<ResourceLocation, Role> ROLES = new OrderedHashMap<>();
     public static final List<ComponentKey<? extends RoleComponent>> COMPONENT_KEYS = new ArrayList<>();
-    public static final Role DISCOVERY_CIVILIAN = registerRole(new NoramlRole(TMM.id("discovery_civilian"), 0x36E51B, true, false, Role.MoodType.NONE, -1, true));
-    public static final Role CIVILIAN = registerRole(new NoramlRole(TMM.id("civilian"), 0x36E51B, true, false, Role.MoodType.REAL, GameConstants.getInTicks(0, 10), false));
-    public static final Role VIGILANTE = registerRole(new NoramlRole(TMM.id("vigilante"), 0x1B8AE5, true, false, Role.MoodType.REAL, GameConstants.getInTicks(0, 10), false).setVigilanteTeam(true));
-    public static final Role KILLER = registerRole(new NoramlRole(TMM.id("killer"), 0xC13838, false, true, Role.MoodType.FAKE, -1, true));
-    public static final Role LOOSE_END = registerRole(new NoramlRole(TMM.id("loose_end"), 0x9F0000, false, false, Role.MoodType.NONE, -1, false));
+    public static final Role DISCOVERY_CIVILIAN = registerRole(
+            new NoramlRole(TMM.id("discovery_civilian"), 0x36E51B, true, false, Role.MoodType.NONE, -1, true));
+    public static final Role CIVILIAN = registerRole(new NoramlRole(TMM.id("civilian"), 0x36E51B, true, false,
+            Role.MoodType.REAL, GameConstants.getInTicks(0, 10), false));
+    public static final Role VIGILANTE = registerRole(new NoramlRole(TMM.id("vigilante"), 0x1B8AE5, true, false,
+            Role.MoodType.REAL, GameConstants.getInTicks(0, 10), false).setVigilanteTeam(true));
+    public static final Role KILLER = registerRole(
+            new NoramlRole(TMM.id("killer"), 0xC13838, false, true, Role.MoodType.FAKE, -1, true));
+    public static final Role LOOSE_END = registerRole(
+            new NoramlRole(TMM.id("loose_end"), 0x9F0000, false, false, Role.MoodType.NONE, -1, false));
 
     public static Role registerRole(Role role) {
         ROLES.put(role.identifier(), role);
-        if (role.getComponentKey()!=null){
+        if (role.getComponentKey() != null) {
             COMPONENT_KEYS.add(role.getComponentKey());
         }
         return role;
     }
+
     public static void addRoleComponents(ComponentKey<? extends RoleComponent> componentKeyToAdd) {
         COMPONENT_KEYS.add(componentKeyToAdd);
     }
